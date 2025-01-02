36cfb2bc653bea25527b7962860e6d4ed30d72fb
Eugene Vorontsov <sejeenn@gmail.com>
2025-01-02 12:10:24 +0300

    сделаны роуты юзеров и твитов

---
+++ src/api/routes_tweets.py
@@ -0,0 +1,31 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Header
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.api.crud.crud_users import get_current_user
+from src.api.crud import crud_tweets
+from src.core.schemas.schema_tweets import TweetListSchema
+from src.core.models.db_helper import db_helper
+from src.core.models.model_users import User
+
+tweets_route = APIRouter(
+    prefix="/api/tweets"
+)
+
+
+@tweets_route.get(
+    "", status_code=200, response_model=TweetListSchema,
+    tags=["Получение твитов, пользователей, на которых подписан текущий пользователь"]
+)
+async def get_tweets_follow_user(
+        session: Annotated[
+            AsyncSession,
+            Depends(db_helper.session_getter),
+        ],
+        current_user: Annotated[User, Depends(get_current_user)],
+        api_key: Annotated[str | None, Header()] = 'test'
+):
+
+    tweets = await crud_tweets.get_all_tweets(session=session, current_user=current_user)
+    return {"tweets": tweets}
---
+++ src/api/routes_users.py
@@ -0,0 +1,63 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Header
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.models.db_helper import db_helper
+from src.core.schemas.schema_users import FullUserSchema, UserReadSchema, CreateUserSchema
+from src.api.crud import crud_users
+
+users_route = APIRouter(
+    prefix="/api/users"
+)
+
+
+@users_route.get(
+    "/me", tags=["Получение данных текущего пользователя"],
+    response_model=FullUserSchema
+)
+async def get_users_me(
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+        api_key:    Annotated[str | None, Header()] = None,
+):
+    user = await crud_users.get_user_by_api_key(session=session, api_key=api_key)
+    data = {"result": "true", "user": {
+        "id": user.id, "name": user.name,
+        "followers": [{"id": u.id, "name": u.name} for u in user.followers],
+        "following": [{"id": u.id, "name": u.name} for u in user.followed]
+    }}
+    return data
+
+
+@users_route.post(
+    "/me",
+    tags=["Создать нового пользователя"],
+    response_model=UserReadSchema
+)
+async def create_user(
+        session: Annotated[
+            AsyncSession,
+            Depends(db_helper.session_getter),
+        ],
+        user_create: CreateUserSchema,
+):
+    user = await crud_users.create_user(session=session, user_create=user_create)
+    return user
+
+
+@users_route.get(
+    "/{user_id}",
+    response_model=FullUserSchema,
+    tags=["Получение данных пользователя по его user_id"]
+)
+async def get_user_by_id(
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+        user_id: int,
+):
+    user = await crud_users.get_user_by_user_id(session=session, user_id=user_id)
+    data = {"result": "true", "user": {
+        "id": user.id, "name": user.name,
+        "followers": [{"id": u.id, "name": u.name} for u in user.followers],
+        "following": [{"id": u.id, "name": u.name} for u in user.followed]
+    }}
+    return data
