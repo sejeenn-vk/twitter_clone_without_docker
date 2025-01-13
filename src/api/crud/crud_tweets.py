07444c9ccaad396c7da27968493a75278982d45f
Eugene Vorontsov <sejeenn@gmail.com>
2025-01-13 16:33:56 +0300

    создан круд создания нового твита

--- src/api/crud/crud_tweets.py
+++ src/api/crud/crud_tweets.py
@@ -3,9 +3,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload, lazyload, defaultload
 
 from src.core.models import User, Tweet, Like
+from src.core.schemas.schema_tweets import TweetInSchema
 
 
-async def get_all_tweets(
+async def get_tweets(
     session: AsyncSession,
     current_user: User,
 ):
@@ -26,7 +27,36 @@ async def get_all_tweets(
         .group_by(Tweet)
         .order_by(desc("likes_count"))
     )
+    # TODO: проблема данного кода в том, что если нет лайков у твита
+    # TODO: твит не появится в ленте новостей
+
     result = await session.execute(stmt)
     tweets = result.unique().scalars().all()
+    print(current_user)
 
     return tweets
+
+
+async def create_tweet(
+    tweet: TweetInSchema,
+    session: AsyncSession,
+    current_user: User,
+):
+    new_tweet = Tweet(tweet_data=tweet.tweet_data, user_id=current_user.id)
+    # Добавляем в индекс, фиксируем, но не записываем в БД!!!
+    session.add(new_tweet)
+    await session.flush()
+
+    # Сохраняем изображения, если есть
+    tweet_media_ids = tweet.tweet_media_ids
+
+    if tweet_media_ids and tweet_media_ids != []:
+        # Привязываем изображения к твиту
+        await ImageService.update_images(
+            tweet_media_ids=tweet_media_ids, tweet_id=new_tweet.id, session=session
+        )
+
+    # Сохраняем в БД все изменения (новый твит + привязку картинок к твиту)
+    await session.commit()
+
+    return new_tweet
