dbbc78db685573c6fc5ab6c0173dde854fcf8732
Eugene Vorontsov <sejeenn@gmail.com>
2025-01-15 17:00:11 +0300

    создан новый файл роутов routes_medias.py

---
+++ src/api/routes_medias.py
@@ -0,0 +1,28 @@
+from typing import Annotated
+
+from fastapi import APIRouter, UploadFile, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.models import db_helper
+from src.core.schemas.schema_images import ImageResponseSchema
+from src.api.crud.crud_images import image_save
+
+medias_route = APIRouter(prefix="/api/medias")
+
+
+@medias_route.post(
+    "",
+    tags=["Загрузка картинки на компьютер"],
+    response_model=ImageResponseSchema,
+    status_code=201,
+)
+async def upload_image(
+    file: UploadFile,
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter),
+    ],
+):
+    # Записываем изображение в файловой системе и создаем запись в БД
+    image_id = await image_save(image_file=file, session=session)
+    return {"media_id": image_id}
