3907c8d122d01f2b7574040149a000672afcc5af
Eugene Vorontsov <sejeenn@gmail.com>
2025-01-15 17:01:27 +0300

    сделано добавление твита с картинкой

--- src/api/crud/crud_tweets.py
+++ src/api/crud/crud_tweets.py
@@ -1,9 +1,11 @@
+import datetime
+
 from sqlalchemy import select, func, desc
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload, lazyload, defaultload
-
+from sqlalchemy.orm import joinedload
 from src.core.models import User, Tweet, Like
 from src.core.schemas.schema_tweets import TweetInSchema
+from src.api.crud.crud_images import update_image
 
 
 async def get_tweets(
@@ -27,12 +29,12 @@ async def get_tweets(
         .group_by(Tweet)
         .order_by(desc("likes_count"))
     )
+
     # TODO: проблема данного кода в том, что если нет лайков у твита
     # TODO: твит не появится в ленте новостей
 
     result = await session.execute(stmt)
     tweets = result.unique().scalars().all()
-    print(current_user)
 
     return tweets
 
@@ -42,20 +44,28 @@ async def create_tweet(
     session: AsyncSession,
     current_user: User,
 ):
-    new_tweet = Tweet(tweet_data=tweet.tweet_data, user_id=current_user.id)
-    # Добавляем в индекс, фиксируем, но не записываем в БД!!!
+    # tweet_data='ц' tweet_media_ids=[] как выглядит ответ от фронта
+    # что нужно вставить в бд
+    # {"content": "Будь здоров!", "user_id": 1, "created_at": datetime.datetime.now()}
+
+    new_tweet = Tweet(
+        content=tweet.tweet_data,
+        user_id=current_user.id,
+        created_at=datetime.datetime.now(),
+    )
+    # # Добавляем в индекс, фиксируем, но не записываем в БД!!!
     session.add(new_tweet)
     await session.flush()
 
-    # Сохраняем изображения, если есть
+    # # Сохраняем изображения, если есть
     tweet_media_ids = tweet.tweet_media_ids
 
     if tweet_media_ids and tweet_media_ids != []:
         # Привязываем изображения к твиту
-        await ImageService.update_images(
+        # для этого нужны: tweet_media_ids, new_tweet.id, session
+        await update_image(
             tweet_media_ids=tweet_media_ids, tweet_id=new_tweet.id, session=session
         )
-
     # Сохраняем в БД все изменения (новый твит + привязку картинок к твиту)
     await session.commit()
 
