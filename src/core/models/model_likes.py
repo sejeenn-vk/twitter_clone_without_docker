a7b6a6cd8df6d12ab0b6ab1eab67cc679ae498a3
Eugene Vorontsov <sejeenn@gmail.com>
2025-01-16 16:59:05 +0300

    исправлены модели изображений и лайков

--- src/core/models/model_likes.py
+++ src/core/models/model_likes.py
@@ -10,11 +10,15 @@ class Like(Base):
     """
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    tweet_id: Mapped[int] = mapped_column(ForeignKey("tweets.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+    tweet_id: Mapped[int] = mapped_column(ForeignKey("tweets.id", ondelete="CASCADE"))
 
-    # Отключаем проверку строк, тем самым убирая уведомление, возникающее при удалении несуществующей строки
+    # Отключаем проверку строк, тем самым убирая уведомление,
+    # возникающее при удалении несуществующей строки
     __mapper_args__ = {"confirm_deleted_rows": False}
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(id={self.id}, tweet_id={self.tweet_id}, user_id={self.user_id})"
+        return (
+            f"{self.__class__.__name__}(id={self.id}, "
+            f"tweet_id={self.tweet_id}, user_id={self.user_id})"
+        )
