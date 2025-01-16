bc6e80df289102a66d61d5064e25195355424175
Eugene Vorontsov <sejeenn@gmail.com>
2025-01-16 16:58:41 +0300

    создан эндпойнт по удалению картинок

--- src/api/routes_tweets.py
+++ src/api/routes_tweets.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, Header
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.api.crud.crud_tweets import delete_tweet
 from src.api.crud.crud_users import get_current_user
 from src.api.crud import crud_tweets
 from src.core.schemas.schema_tweets import TweetListSchema, TweetInSchema
@@ -50,3 +51,24 @@ async def create_new_tweet(
         tweet=tweet, current_user=current_user, session=session
     )
     return {"tweet_id": new_tweet.id}
+
+
+@tweets_route.delete(
+    "/{tweet_id}", status_code=200, tags=["Удаление твита по его tweet_id"]
+)
+async def delete_tweet_by_tweet_id(
+    tweet_id: int,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    """
+    В этом endpoint мы должны убедиться, что пользователь удаляет
+    именно свой собственный твит.
+    :param tweet_id:
+    :param session:
+    :param current_user:
+    :return: {"result": True}
+    """
+    await delete_tweet(user=current_user, tweet_id=tweet_id, session=session)
+
+    return {"result": True}
