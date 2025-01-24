770b6d1d731972e239346b8cb20e493814618eb9
Eugene Vorontsov <sejeenn@gmail.com>
2025-01-24 08:50:12 +0300

    переделаны тесты пользователя

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,19 +1,23 @@
 import pytest
 from httpx import AsyncClient
 
+good_response_user = {
+    "result": True,
+    "user": {
+        "id": 1,
+        "name": "Test User",
+        "followers": [{"id": 2, "name": "Тестовый Пользователь"}],
+        "following": [{"id": 2, "name": "Тестовый Пользователь"}],
+    },
+}
+
 
 @pytest.mark.asyncio(loop_scope="module")
 async def test_users_me(ac: AsyncClient):
     response = await ac.get("/api/users/me", headers={"api-key": "test_1"})
     data = response.json()
     assert response.status_code == 200
-    assert data['result'] == 1
-    assert data['user']['id'] == 1
-    assert data['user']['name'] == 'Test User'
-    assert data['user']['followers'][0]['id'] == 2
-    assert data['user']['following'][0]['id'] == 2
-    assert data['user']['followers'][0]['name'] == 'Тестовый Пользователь'
-    assert data['user']['following'][0]['name'] == 'Тестовый Пользователь'
+    assert data == good_response_user
 
 
 @pytest.mark.asyncio(loop_scope="module")
@@ -21,9 +25,7 @@ async def test_user_by_id(ac: AsyncClient) -> None:
     response = await ac.get("/api/users/1")
     data = response.json()
     assert response.status_code == 200
-    assert data['result'] == 1
-    assert data['user']['id'] == 1
-    assert data['user']['name'] == 'Test User'
+    assert data == good_response_user
 
 
 @pytest.mark.asyncio(loop_scope="module")
@@ -31,7 +33,7 @@ async def test_add_follow(ac: AsyncClient) -> None:
     response = await ac.post("/api/users/1/follow", headers={"api-key": "test_1"})
     data = response.json()
     assert response.status_code == 200
-    assert data['result'] == 1
+    assert data["result"] == 1
 
 
 @pytest.mark.asyncio(loop_scope="module")
@@ -39,7 +41,4 @@ async def test_delete_follow(ac: AsyncClient) -> None:
     response = await ac.delete("/api/users/1/follow", headers={"api-key": "test_2"})
     data = response.json()
     assert response.status_code == 200
-    assert data['result'] == 1
-
-
-
+    assert data["result"] == 1
